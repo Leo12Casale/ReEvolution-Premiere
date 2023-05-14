3dbd37681a752fe578f0190bd0c2f82c089c55ba
LeonelCasale <leonelcasale@gmail.com>
2023-05-14 19:17:06 -0300

    Cambios en BDD según revisiones

--- BDD/Script creación DB.sql
+++ BDD/Script creación DB.sql
@@ -4,18 +4,30 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-
 -- -----------------------------------------------------
 -- Schema reevolutionpremiere
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `reevolutionpremiere` ;
-CREATE SCHEMA IF NOT EXISTS `reevolutionpremiere` DEFAULT CHARACTER SET utf8 ;
-USE `reevolutionpremiere`;
 
 -- -----------------------------------------------------
--- Table `reevolutionpremiere`.`emisor_facturas`
+-- Schema reevolutionpremiere
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`emisor_facturas` (
+CREATE SCHEMA IF NOT EXISTS `reevolutionpremiere` DEFAULT CHARACTER SET utf8 ;
+-- -----------------------------------------------------
+-- Schema reevolutionpremiere
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `reevolutionpremiere` ;
+
+-- -----------------------------------------------------
+-- Schema reevolutionpremiere
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `reevolutionpremiere` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `reevolutionpremiere` ;
+
+-- -----------------------------------------------------
+-- Table `reevolutionpremiere`.`a_emisor_facturas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`a_emisor_facturas` (
   `nombre` VARCHAR(60) NOT NULL,
   `telefono` VARCHAR(60) NOT NULL,
   `email` VARCHAR(60) NOT NULL,
@@ -31,12 +43,11 @@ CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`emisor_facturas` (
   PRIMARY KEY (`nombre`))
 ENGINE = InnoDB;
 
-USE `reevolutionpremiere` ;
 
 -- -----------------------------------------------------
--- Table `reevolutionpremiere`.`garante`
+-- Table `reevolutionpremiere`.`a_garante`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`garante` (
+CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`a_garante` (
   `dni` INT NOT NULL,
   `nombre` VARCHAR(60) NOT NULL DEFAULT '',
   `telefono` VARCHAR(45) NOT NULL DEFAULT '',
@@ -53,9 +64,9 @@ DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `reevolutionpremiere`.`cliente_asociado`
+-- Table `reevolutionpremiere`.`a_cliente_asociado`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`cliente_asociado` (
+CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`a_cliente_asociado` (
   `codigo_usuario` INT NOT NULL,
   `dni` INT NOT NULL,
   `nombre` VARCHAR(60) NOT NULL,
@@ -66,13 +77,6 @@ CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`cliente_asociado` (
   `provincia` VARCHAR(45) NOT NULL DEFAULT '',
   `codigo_postal` INT NULL DEFAULT NULL,
   `observaciones` VARCHAR(45) NOT NULL DEFAULT '',
-  `sueldo` FLOAT NOT NULL DEFAULT 0,
-  `limite_de_credito` FLOAT NULL DEFAULT NULL,
-  `limite_por_compra` FLOAT NULL,
-  `saldo_anterior` FLOAT NOT NULL DEFAULT '0',
-  `saldo_mes_anterior` FLOAT NOT NULL DEFAULT 0,
-  `recargos_mora` FLOAT NOT NULL DEFAULT 0,
-  `consumo` FLOAT NOT NULL DEFAULT '0',
   `dni_garante` INT NULL DEFAULT NULL,
   `borrado` BIT(1) NOT NULL DEFAULT b'0',
   PRIMARY KEY (`codigo_usuario`),
@@ -80,15 +84,71 @@ CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`cliente_asociado` (
   INDEX `USUARIO_FK_Garante_idx` (`dni_garante` ASC) VISIBLE,
   CONSTRAINT `cliente_asociado_FK_Garante`
     FOREIGN KEY (`dni_garante`)
-    REFERENCES `reevolutionpremiere`.`garante` (`dni`))
+    REFERENCES `reevolutionpremiere`.`a_garante` (`dni`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `reevolutionpremiere`.`adicional`
+-- Table `reevolutionpremiere`.`h_cierre_mensual_cliente_asociado`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`adicional` (
+CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`h_cierre_mensual_cliente_asociado` (
+  `id_cierre` INT NOT NULL AUTO_INCREMENT,
+  `fecha_cierre` DATE NULL,
+  `mes_cierre` INT NOT NULL,
+  `consumo` FLOAT NOT NULL,
+  `recargos_mora` FLOAT NOT NULL,
+  `saldo_mes_anterior` FLOAT NOT NULL,
+  `pago_mes_anterior` FLOAT NOT NULL,
+  `codigo_usuario` INT NOT NULL,
+  PRIMARY KEY (`id_cierre`),
+  INDEX `h_cierre_mensual_cliente_asociado_FK_cliente_asociado_idx` (`codigo_usuario` ASC) VISIBLE,
+  UNIQUE INDEX `id_cierre_UNIQUE` (`id_cierre` ASC) VISIBLE,
+  CONSTRAINT `h_cierre_mensual_cliente_asociado_FK_cliente_asociado`
+    FOREIGN KEY (`codigo_usuario`)
+    REFERENCES `reevolutionpremiere`.`a_cliente_asociado` (`codigo_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `reevolutionpremiere`.`a_categoria_comercio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`a_categoria_comercio` (
+  `id_categoria` INT NOT NULL AUTO_INCREMENT,
+  `nombre_categoria` VARCHAR(45) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id_categoria`),
+  UNIQUE INDEX `id_categoria_UNIQUE` (`id_categoria` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `reevolutionpremiere`.`a_limites_cliente_asociado`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`a_limites_cliente_asociado` (
+  `id_limite` INT NOT NULL AUTO_INCREMENT,
+  `fecha_actualizacion` DATE NOT NULL,
+  `sueldo` FLOAT NOT NULL,
+  `limite_credito` FLOAT NOT NULL,
+  `limite_compra` FLOAT NOT NULL,
+  `codigo_usuario` INT NOT NULL,
+  PRIMARY KEY (`id_limite`),
+  UNIQUE INDEX `id_limite_UNIQUE` (`id_limite` ASC) VISIBLE,
+  INDEX `a_limites_cliente_asociado_idx` (`codigo_usuario` ASC) VISIBLE,
+  CONSTRAINT `a_limites_cliente_asociado`
+    FOREIGN KEY (`codigo_usuario`)
+    REFERENCES `reevolutionpremiere`.`a_cliente_asociado` (`codigo_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+USE `reevolutionpremiere` ;
+
+-- -----------------------------------------------------
+-- Table `reevolutionpremiere`.`a_adicional`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`a_adicional` (
   `dni` INT NOT NULL,
   `nombre` VARCHAR(50) NOT NULL DEFAULT '',
   `telefono` VARCHAR(45) NOT NULL DEFAULT '',
@@ -103,15 +163,15 @@ CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`adicional` (
   INDEX `adicional_FK_usuario_idx` (`codigo_cliente_asociado_titular` ASC) VISIBLE,
   CONSTRAINT `adicional_FK_usuario`
     FOREIGN KEY (`codigo_cliente_asociado_titular`)
-    REFERENCES `reevolutionpremiere`.`cliente_asociado` (`codigo_usuario`))
+    REFERENCES `reevolutionpremiere`.`a_cliente_asociado` (`codigo_usuario`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `reevolutionpremiere`.`cierrecaja`
+-- Table `reevolutionpremiere`.`h_cierrecaja`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`cierrecaja` (
+CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`h_cierrecaja` (
   `fecha_cierre` DATE NOT NULL,
   `importe_efectivo` FLOAT NOT NULL,
   `importe_tarjeta` FLOAT NOT NULL,
@@ -122,9 +182,9 @@ DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `reevolutionpremiere`.`comercio`
+-- Table `reevolutionpremiere`.`a_comercio`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`comercio` (
+CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`a_comercio` (
   `codigo_comercio` INT NOT NULL,
   `razon_social` VARCHAR(60) NOT NULL,
   `nombre_fantasia` VARCHAR(60) NOT NULL DEFAULT '',
@@ -143,16 +203,23 @@ CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`comercio` (
   `observaciones` VARCHAR(100) NOT NULL DEFAULT '',
   `borrado` BIT(1) NOT NULL DEFAULT b'0',
   `comisiones_por_cuota` JSON NOT NULL,
+  `id_categoria` INT NULL,
   PRIMARY KEY (`codigo_comercio`),
-  UNIQUE INDEX `ID_UNIQUE` (`codigo_comercio` ASC) VISIBLE)
+  UNIQUE INDEX `ID_UNIQUE` (`codigo_comercio` ASC) VISIBLE,
+  INDEX `a_comercio_FK_id_categoria_idx` (`id_categoria` ASC) VISIBLE,
+  CONSTRAINT `a_comercio_FK_id_categoria`
+    FOREIGN KEY (`id_categoria`)
+    REFERENCES `reevolutionpremiere`.`a_categoria_comercio` (`id_categoria`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `reevolutionpremiere`.`cupon`
+-- Table `reevolutionpremiere`.`h_cupon`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`cupon` (
+CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`h_cupon` (
   `numero` INT NOT NULL,
   `codigo_cliente_asociado` INT NOT NULL,
   `dni_adicional` INT NULL DEFAULT NULL,
@@ -171,13 +238,13 @@ CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`cupon` (
   INDEX `cupones_adicional_FK_idx` (`dni_adicional` ASC) VISIBLE,
   CONSTRAINT `cupones_IDComercios_FK`
     FOREIGN KEY (`codigo_comercio`)
-    REFERENCES `reevolutionpremiere`.`comercio` (`codigo_comercio`),
+    REFERENCES `reevolutionpremiere`.`a_comercio` (`codigo_comercio`),
   CONSTRAINT `cupones_IDUsuarios_FK`
     FOREIGN KEY (`codigo_cliente_asociado`)
-    REFERENCES `reevolutionpremiere`.`cliente_asociado` (`codigo_usuario`),
+    REFERENCES `reevolutionpremiere`.`a_cliente_asociado` (`codigo_usuario`),
   CONSTRAINT `cupones_adicional_FK`
     FOREIGN KEY (`dni_adicional`)
-    REFERENCES `reevolutionpremiere`.`adicional` (`dni`)
+    REFERENCES `reevolutionpremiere`.`a_adicional` (`dni`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -185,9 +252,9 @@ DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `reevolutionpremiere`.`factura_cliente_asociado`
+-- Table `reevolutionpremiere`.`h_factura_cliente_asociado`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`factura_cliente_asociado` (
+CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`h_factura_cliente_asociado` (
   `numero_factura` INT NOT NULL,
   `tipo_factura` CHAR(1) NOT NULL DEFAULT 'B',
   `codigo_tipo_factura` VARCHAR(3) NOT NULL DEFAULT '006',
@@ -213,15 +280,15 @@ CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`factura_cliente_asociado` (
   INDEX `fk_factura_usuario_idx` (`codigo_cliente_asociado` ASC) VISIBLE,
   CONSTRAINT `fk_facturausuario_clienteasociado`
     FOREIGN KEY (`codigo_cliente_asociado`)
-    REFERENCES `reevolutionpremiere`.`cliente_asociado` (`codigo_usuario`))
+    REFERENCES `reevolutionpremiere`.`a_cliente_asociado` (`codigo_usuario`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `reevolutionpremiere`.`factura_comercio`
+-- Table `reevolutionpremiere`.`h_factura_comercio`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`factura_comercio` (
+CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`h_factura_comercio` (
   `numero_factura` INT NOT NULL,
   `tipo_factura` CHAR(1) NOT NULL,
   `codigo_tipo_factura` VARCHAR(3) NOT NULL,
@@ -242,15 +309,15 @@ CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`factura_comercio` (
   INDEX `fk_facturac_comercio_idx` (`id_comercio` ASC) VISIBLE,
   CONSTRAINT `fk_facturacomercio_comercio1`
     FOREIGN KEY (`id_comercio`)
-    REFERENCES `reevolutionpremiere`.`comercio` (`codigo_comercio`))
+    REFERENCES `reevolutionpremiere`.`a_comercio` (`codigo_comercio`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `reevolutionpremiere`.`cuota`
+-- Table `reevolutionpremiere`.`h_cuota`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`cuota` (
+CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`h_cuota` (
   `id_cuota` INT NOT NULL AUTO_INCREMENT,
   `numero_cuota` INT NOT NULL,
   `importe` FLOAT NOT NULL,
@@ -266,13 +333,13 @@ CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`cuota` (
   INDEX `cuota_fk_facturacomercio_idx` (`numero_factura_comercio` ASC) VISIBLE,
   CONSTRAINT `cuota_fk_IDCupon`
     FOREIGN KEY (`id_cupon`)
-    REFERENCES `reevolutionpremiere`.`cupon` (`numero`),
+    REFERENCES `reevolutionpremiere`.`h_cupon` (`numero`),
   CONSTRAINT `cuota_fk_facturaclienteadicional`
     FOREIGN KEY (`numero_factura_cliente_asociado`)
-    REFERENCES `reevolutionpremiere`.`factura_cliente_asociado` (`numero_factura`),
+    REFERENCES `reevolutionpremiere`.`h_factura_cliente_asociado` (`numero_factura`),
   CONSTRAINT `cuota_fk_facturacomercio`
     FOREIGN KEY (`numero_factura_comercio`)
-    REFERENCES `reevolutionpremiere`.`factura_comercio` (`numero_factura`)
+    REFERENCES `reevolutionpremiere`.`h_factura_comercio` (`numero_factura`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -281,9 +348,9 @@ DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `reevolutionpremiere`.`movimiento`
+-- Table `reevolutionpremiere`.`h_movimiento`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`movimiento` (
+CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`h_movimiento` (
   `id_movimiento` INT NOT NULL AUTO_INCREMENT,
   `concepto` VARCHAR(30) NULL DEFAULT 'Varios',
   `modo_pago` VARCHAR(15) NOT NULL,
@@ -299,10 +366,10 @@ CREATE TABLE IF NOT EXISTS `reevolutionpremiere`.`movimiento` (
   INDEX `movimiento_FK_factura_comercio` (`numero_factura_comercio` ASC) VISIBLE,
   CONSTRAINT `movimiento_FK_codigo_usuario`
     FOREIGN KEY (`codigo_usuario`)
-    REFERENCES `reevolutionpremiere`.`cliente_asociado` (`codigo_usuario`),
+    REFERENCES `reevolutionpremiere`.`a_cliente_asociado` (`codigo_usuario`),
   CONSTRAINT `movimiento_FK_factura_comercio`
     FOREIGN KEY (`numero_factura_comercio`)
-    REFERENCES `reevolutionpremiere`.`factura_comercio` (`numero_factura`))
+    REFERENCES `reevolutionpremiere`.`h_factura_comercio` (`numero_factura`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 2
 DEFAULT CHARACTER SET = utf8mb3;
