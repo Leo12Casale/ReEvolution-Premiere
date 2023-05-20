f2fd2a6ea15166de63a8c009b4a6a7cfed5ee3b4
LeonelCasale <leonelcasale@gmail.com>
2023-05-20 12:37:21 -0300

    Creación proyecto

---
+++ ReEvolutionPremiere/src/test/java/com/reevolutionpremiere/ReEvolutionPremiere/ReEvolutionPremiereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reevolutionpremiere.ReEvolutionPremiere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReEvolutionPremiereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
